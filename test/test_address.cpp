2ce3f637a295e83769e7f161a9d4b0944315bd1f
Peter Ferenc Hajdu <peter.ferenc.hajdu@gmail.com>
2014-06-25 17:49:38 +0200

    fix error with unknown initialization sequence of global variables

--- test/test_address.cpp
+++ test/test_address.cpp
@@ -5,6 +5,11 @@ using namespace igloo;
 
 Describe( an_address )
 {
+  void SetUp()
+  {
+    full_tcp_address.reset( new the::net::Address( "tcp://localhost.something:1234" ) );
+    address_without_protocol.reset( new the::net::Address( "localhost.something:1234" ) );
+  }
 
   void test_host_of( const the::net::Address& address )
   {
@@ -18,35 +23,35 @@ Describe( an_address )
 
   It( provides_host_part_of_address )
   {
-    test_host_of( full_tcp_address );
+    test_host_of( *full_tcp_address );
   }
 
   It( provides_port_part_of_address )
   {
-    test_port_of( full_tcp_address );
+    test_port_of( *full_tcp_address );
   }
 
   It( provides_protocol_part_of_address )
   {
-    AssertThat( full_tcp_address.protocol, Equals( "tcp" ) );
+    AssertThat( full_tcp_address->protocol, Equals( "tcp" ) );
   }
 
   It( provides_host_part_of_address_without_protocol )
   {
-    test_host_of( address_without_protocol );
+    test_host_of( *address_without_protocol );
   }
 
   It( provides_port_part_of_address_without_protocol )
   {
-    test_port_of( address_without_protocol );
+    test_port_of( *address_without_protocol );
   }
 
   It( provides_protocol_part_of_address_without_protocol )
   {
-    AssertThat( address_without_protocol.protocol, Equals( "" ) );
+    AssertThat( address_without_protocol->protocol, Equals( "" ) );
   }
 
-  the::net::Address full_tcp_address{ "tcp://localhost.something:1234" };
-  the::net::Address address_without_protocol{ "localhost.something:1234" };
+  std::unique_ptr< the::net::Address > full_tcp_address;
+  std::unique_ptr< the::net::Address > address_without_protocol;
 };
 
